388523b3464259357680e8b423882686cefcf655
Varsha BR <brvarsha.xworkz@gmail.com>
2023-07-20 11:16:04 +0530

    first prgm

---
+++ Bag.java
@@ -0,0 +1,15 @@
+class Bag{
+	public static void main(String[] args){
+	System.out.println("bag");
+	String brand="Puma";
+	String color="black";
+	long cost=1500;
+	int warranty=2;
+	char size='M';
+	System.out.println("brand:" +brand);
+	System.out.println("color:" +color);
+	System.out.println("cost:" +cost);
+	System.out.println("warranty:" +warranty);
+	System.out.println("size:" +size);
+	}
+}
