f0c080713890fa108d237e0baabc652f8a32e8a0
Varsha BR <brvarsha.xworkz@gmail.com>
2023-08-01 00:22:36 +0530

    Assosiation1

---
+++ practice/src/practice/app/Lift.java
@@ -0,0 +1,19 @@
+package practice.app;
+
+public class Lift {
+	public int number;
+
+	public Lift(int number) 
+	{
+      this.number = number;
+	}
+
+	public void printInfo() {
+			System.out.println("Invoking lift info");
+
+			System.out.println("No :" + number);
+	}
+	
+}
+
+
---
+++ practice/src/practice/app/Sport.java
@@ -0,0 +1,26 @@
+package practice.app;
+
+public class Sport {
+	public String name;
+	public Player[] players;
+
+	public Sport(String name, Player[] players) {
+		this.name = name;
+		this.players = players;
+
+	}
+
+	public void printInfo() {
+		
+		System.out.println("Printing sports info");
+		System.out.println("Name :" +name);
+
+		for (int sport = 0; sport < this.players.length; sport++) {
+			Player play = players[sport];
+			play.printInfo();
+		}
+	}
+}
+
+
+
